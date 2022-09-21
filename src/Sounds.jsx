d0ee802cbb050a9002e3e18f82a7e7b856b5bcc8
zanlyons <zanlyons@hotmail.com>
2022-09-21 15:01:54 +0200

    sounds wavs

---
+++ src/Sounds.jsx
@@ -0,0 +1,36 @@
+export default function Sounds() {
+    return ( 
+
+
+       
+            
+
+        <div class="drum-pads">
+            <div class="key drum-pad"></div>
+            <div class="key drum-pad"></div>
+            <div class="key drum-pad"></div>
+            <div class="key drum-pad"></div>
+            <div class="key drum-pad"></div>
+            <div class="key drum-pad"></div>
+            <div class="key drum-pad"></div>
+            <div class="key drum-pad"></div>
+            <div class="key drum-pad"></div>
+            
+
+        </div>
+    );
+   }
+
+   const Sounds = [SoundDrums, ]
+
+const SoundDrums = [
+    {name: 'drum1', path: "src/assets/sounds/drums/K01DHit-01.wav"},
+    {name: 'drum2', path: "src/assets/sounds/drums/K01DHit-02.wav"},
+    {name: 'drum3', path: "src/assets/sounds/drums/K01DHit-03.wav"}
+    {name: 'drum4', path: "src/assets/sounds/drums/K01DHit-04.wav"}
+    {name: 'drum5', path: "src/assets/sounds/drums/K01DHit-05.wav"}
+    {name: 'drum6', path: "src/assets/sounds/drums/K01DHit-06.wav"}
+    {name: 'drum7', path: "src/assets/sounds/drums/K01DHit-07.wav"}
+    {name: 'drum8', path: "src/assets/sounds/drums/K01DHit-08.wav"}
+    {name: 'drum9', path: "src/assets/sounds/drums/K01DHit-09.wav"}
+]
