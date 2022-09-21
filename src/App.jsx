9973ff3ec6ed213aa06b7c9838002680ac0bc0d2
David <callmelmao@gmail.com>
2022-09-21 16:23:38 +0100

    same as previous, but fixed

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ export default function App() {
         onFilterSoundChange={setFilterSound}
       />
       <div>
-  		{/*<Sounds />*/}
+  		<Sounds />
         <Composers />
       </div>
     </div>
