99a01a7be20e3f2a2259cf75624284b596a9d5a3
LubenStoyanov <luben.stoyanov.ls@gmail.com>
2022-09-21 12:18:28 +0200

    Add state functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
+import Searchbar from "./Searchbar.jsx";
 import "./App.css";
 
 export default function App() {
+  const [filterSound, setFilterSound] = useState("");
+
   return (
     <div className="App">
-      <Navbar />
-      <Searchbar />
-      <div>
+      {/* <Navbar /> */}
+      <Searchbar
+        filterSound={filterSound}
+        onFilterSoundChange={setFilterSound}
+      />
+      {/* <div>
         <Sounds />
         <Composers />
-      </div>
+      </div> */}
     </div>
   );
 }
