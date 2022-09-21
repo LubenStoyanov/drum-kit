625845cc2accc24de1438f48e7d88f51cb5ee5bc
‘Katrin <katrin.baier77@gmail.com>
2022-09-21 12:27:52 +0200

    navigation-first-push

---
+++ src/Navbar.jsx
@@ -0,0 +1,18 @@
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            <nav>
+                <ul>
+                    <li>All</li>
+                    <li>Drums</li>
+                    <li>Piano</li>
+                    <li>Violin</li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar
