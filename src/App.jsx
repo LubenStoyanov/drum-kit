f3401cad6d198d67e298c715842f28d6b25482af
David <callmelmao@gmail.com>
2022-09-23 12:43:44 +0100

    pulling stuff from github

--- src/App.jsx
+++ src/App.jsx
@@ -22,41 +22,41 @@ export default function App() {
   const [button] = useState(categories);
 
   const filter = (button) =>{
-    if(button === 'All') {
-      setMenuItem(items);
-      return;
-    }
-    const filteredData = items.filter (item => item.category === button);
-    setMenuItem(filteredData)
+	if(button === 'All') {
+	  setMenuItem(items);
+	  return;
+	}
+	const filteredData = items.filter (item => item.category === button);
+	setMenuItem(filteredData)
   }
   
   // lubens filter
   const filteredSounds = sounds
-    .filter((sound) => sound.name === filterSound)
-    .map((sound) => <div key={sound.id}>{sound.file}</div>);
+	.filter((sound) => sound.name === filterSound)
+	.map((sound) => <div key={sound.id}>{sound.file}</div>);
 
   return (
-    <div className="App">
-      <h1>Play your sound</h1>
-      <div className="flex-container">
-        <Button button={button} filter={filter} />
-      </div>
-      <div className="flex-container">
-        <Searchbar
-          filterSound={filterSound}
-          onFilterSoundChange={setFilterSound}
-        />
-          {filteredSounds}
-      </div>
-      <div className="flex-container">
-        <div className="flex-container-80">
-        <SoundsKat menuItem={menuItem} />
-      {/* <Sounds /> */}
-      </div>
-      <div className="flex-container-20">
-        <Composers />
-      </div>
-      </div>
-    </div>
+	<div className="App">
+	  <h1>Play your sound</h1>
+	  <div className="flex-container">
+		<Button button={button} filter={filter} />
+	  </div>
+	  <div className="flex-container">
+		<Searchbar
+		  filterSound={filterSound}
+		  onFilterSoundChange={setFilterSound}
+		/>
+		  {filteredSounds}
+	  </div>
+	  <div className="flex-container">
+		<div className="flex-container-80">
+		<SoundsKat menuItem={menuItem} />
+	  {/* <Sounds /> */}
+	  </div>
+	  <div className="flex-container-20">
+		<Composers />
+	  </div>
+	  </div>
+	</div>
   );
 }
