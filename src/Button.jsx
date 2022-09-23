f07d585e6c096392ccd5720d269a058e7ff188e5
LubenStoyanov <luben.stoyanov.ls@gmail.com>
2022-09-23 18:20:14 +0200

    Rewrite filter, audio and data set

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 
-function Button({button, filter}) {
-    return (
-        <>
-            {button.map((cat, i)=>{
-                return <div className="button-container">
-                        <button className="button" type="button" onClick={()=>filter(cat)}>
-                            <ul>
-                                <li>{cat}</li>
-                            </ul>
-                        </button>
-                    </div>
-                })
-            }       
-     </>
-    )
+function Button({ button, filter }) {
+  return (
+    <>
+      {button.map((cat, i) => {
+        return (
+          <div className="button-container">
+            <button
+              className="button"
+              type="button"
+              onClick={() => filter(cat)}
+            >
+              <ul>
+                <li>{cat}</li>
+              </ul>
+            </button>
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default Button;
